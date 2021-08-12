fcb780ed5860977ce609545d7cc70e3ad4e820d9
Gbadamson <gbadamosi.abubakar.95@gmail.com>
2021-08-12 04:44:07 +0100

    adding some touches and comment

--- learner.js
+++ learner.js
@@ -1,8 +1,13 @@
+//Instantiating the express framework
 const express = require("express");
+//creating a dummy routing port
 const port = 2221;
+//Calling on the class of file sytem, a module in the express framework
 const fs = require("fs");
+//Initailizing the instance of the class of express
 const app = express();
 
+//this is to do a get request from the server
 app.get("/", (req, res) => {
   res.send("Hello User, this is a fun project");
 });
@@ -15,6 +20,8 @@ app.get("/todo", (req, res) => {
     return res.json({ todo: todo });
   });
 });
+//We are creating this so that we can be able to update a file on the todo server
+app.put("");
 
 app.listen(port, () => {
   console.log("We are doing fine");
