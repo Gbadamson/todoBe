39ea49b6277f25aad48073bc92d90db9e415a760
Gbadamson <gbadamosi.abubakar.95@gmail.com>
2021-08-12 04:23:20 +0100

    additional changes

--- learner.js
+++ learner.js
@@ -1,9 +1,19 @@
 const express = require("express");
-const port = 22311;
+const port = 2221;
+const fs = require("fs");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello User, this is a fun project");
+});
+app.get("/todo", (req, res) => {
+  fs.readFile("./Store/todo.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("Oops! something seems not to be right");
+    }
+    const todo = JSON.parse(data);
+    return res.json({ todo: todo });
+  });
 });
 
 app.listen(port, () => {
